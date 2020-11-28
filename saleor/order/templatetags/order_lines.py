cbaf0da9cf702bf476ff7db950a6bdbfc5f12cfc
Oleksandr Melnykov <oleksandr.melnykov.p@gmail.com>
2020-11-28 13:09:32 +0200

    Update emails

--- saleor/order/templatetags/order_lines.py
+++ saleor/order/templatetags/order_lines.py
@@ -16,6 +16,12 @@ def display_translated_order_line_name(order_line: OrderLine):
 
 
 @register.simple_tag()
+def display_translated_product_name(order_line: OrderLine):
+    product_name = order_line.translated_product_name or order_line.product_name
+    return f"{product_name}"
+
+
+@register.simple_tag()
 def order_line_thumbnail(order_line: OrderLine, size=255):
     try:
         product_image = order_line.variant.get_first_image()
